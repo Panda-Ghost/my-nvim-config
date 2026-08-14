2c292cd0677162c7c2e543078230f07db555efa2
Panda-Ghost <billy5902@outlook.com>
2026-08-14 14:32:01 +0800

    upd

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,11 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
-vim.o.tabstop = 4
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
 vim.o.relativenumber = false
 vim.g.autoformat = false
+vim.o.foldmethod = "marker"
+vim.o.foldlevel = 0
+vim.o.foldlevelstart = 0
+
 
 vim.api.nvim_create_autocmd({ "BufWrite" }, {
     pattern = { "*" },
---
+++ lua/plugins/folds.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { folds = { enable = false } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = { folds = { enabled = false } },
+  },
+}
